928cd60078811450a3af908e41333531b478beef
Josh English <joshua.english@fitchsolutions.com>
2018-02-06 08:28:19 -0600

    First linear regression with estimators

---
+++ m4-LinearRegressionWithEstimators.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn import datasets, linear_model
+
+from returns_data import read_goog_sp500_data
+xData, yData = read_goog_sp500_data()
+
+googModel = linear_model.LinearRegression()
+
+googModel.fit(xData.reshape(-1, 1), yData.reshape(-1, 1))
+
+print(googModel.coef_)
+print(googModel.intercept_)
+
+# Simple Linear Regression with tf.contrib.learn
+
+import tensorflow as tf
+
+
+features = [tf.contrib.layers.real_valued_column('x', dimension=1)]
+
+estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
+
+input_fn = tf.contrib.learn.io.numpy_input_fn({'x': xData}, yData, batch_size=len(xData), num_epochs=100000)
+
+fit = estimator.fit(input_fn=input_fn, steps=100000)
+
+for variable_name in fit.get_variable_names():
+    print(variable_name, '--->', fit.get_variable_value(variable_name))
